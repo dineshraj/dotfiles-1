c623e1cc1a02e4b211cb863270f9d7d1cf04598c
Dineshraj Goomany <dineshraj.goomany@gmail.com>
2022-03-02 19:04:24 +0000

    Update run.sh
    Update directory to be .dotfiles

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 if type git > /dev/null; then
-    git clone https://github.com/dineshraj/dotfiles ~/dotfiles
-    chmod +x ~/dotfiles
-    cd ~/dotfiles
+    git clone https://github.com/dineshraj/dotfiles ~/.dotfiles
+    chmod +x ~/.dotfiles
+    cd ~/.dotfiles
     ./install.sh
 else
     curl -LO https://github.com/dineshraj/dotfiles/archive/main.zip
-    unzip master.zip
-    rm -rf master.zip
-    mv dotfiles-master ~/dotfiles
+    unzip main.zip
+    rm -rf main.zip
+    mv dotfiles-main ~/.dotfiles
     chmod +x ~/dotfiles
     cd ~/dotfiles
     ./install.sh
